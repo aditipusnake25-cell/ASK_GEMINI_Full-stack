daee004c9c7f9aeeb077d843ef9a5ec37449145e
Your Name <your_email@example.com>
2026-09-03 14:14:43 +0530

    changed

--- asking-gemini/backend/server.js
+++ asking-gemini/backend/server.js
@@ -1,17 +1,34 @@
 require("dotenv").config();
-const askRoutes = require("./routes/askRoutes");
+
 const express = require("express");
 const path = require("path");
+const askRoutes = require("./routes/askRoutes");
+
 const app = express();
+
 app.use(express.json());
+
+// Frontend
 app.use(express.static(path.join(__dirname, "../frontend")));
-const PORT = process.env.PORT || 5000;
+
+// API routes
 app.use("/api", askRoutes);
+
+// API 404
 app.use("/api", (req, res) => {
   res.status(404).json({
     error: "API route not found",
   });
 });
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+// For Vercel
+module.exports = app;
+
+// For local development
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
